d853f8ed6fcf4452f3dade38a9f5d35872ef970e
GahhSam <gabrielmourao2055@gmail.com>
2024-01-24 17:25:02 -0300

    Exercício de Estruturas Condicionais

---
+++ Exercício de Estruturas Condicionais.js
@@ -0,0 +1,14 @@
+let num = prompt("Digite um número:");
+
+if (num > 0 && num>=100) {
+    console.log("O número é positivo e maior que 100.");
+} 
+else if (num < 0) {
+    console.log("O número é negativo.");
+}
+ else if (num >100) {
+    console.log("O número é negativo.");
+}
+else {
+    console.log("O número é zero.");
+}
