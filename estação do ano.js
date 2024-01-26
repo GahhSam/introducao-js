cd3262ac9be6c170564741c369778809f2120b9d
GahhSam <gabrielmourao2055@gmail.com>
2024-01-26 16:41:28 -0300

    Estação do ano

---
+++ estação do ano.js
@@ -0,0 +1,19 @@
+let estacaoDoAno = prompt("Digite uma estação do ano:");
+
+switch(estacaoDoAno) {
+case 'verao':
+console.log('Estamos no verão!');
+break;
+case 'inverno':
+console.log('Estamos no inverno!');
+break;
+case 'outono':
+console.log('Estamos no outono!');
+break;
+case 'primavera':
+console.log('Estamos no primavera!');
+break;
+default:
+console.log('Esta estação do ano não existe!');
+break;
+}
