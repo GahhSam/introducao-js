6ea4a2911a3798c1477a7040ae0c5cc6480b4f11
GahhSam <gabrielmourao2055@gmail.com>
2024-02-02 17:59:40 -0300

    Bah

--- Tela de Login/Tela de login.js
+++ Tela de Login/Tela de login.js
@@ -1,17 +1,17 @@
 function login() {
-    const senha = document.querySelector('.senha');
-	const btn = document.querySelector('.btn');
+    const senha = document.querySelector('email');
+	const btn = document.querySelector('password');
 
 	btn.onclick = () =>
 	{
 		if (senha.type === 'password') 
 		{
-			senha.type = 'text'
+			email.type = 'text'
 			btn.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8SA14-A-zoRaiJ2GdUiESsisaFiHNYrUZtjtjZqnth0D_KdfkwzQWIdCjbzhAoYKPTvs&usqp=CAU'
 		}
 		else
 		{
-			senha.type = 'password'
+			password.type = 'password'
 			btn.src = 'https://icon-library.com/images/icon-eyes/icon-eyes-12.jpg'
 		}
 	}}
